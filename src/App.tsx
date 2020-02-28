177ff216989dec1b41db8c6b0e51e5f4314c0e84
moru <e1028092@student.tuwien.ac.at>
2020-02-28 11:40:07 +0100

    Added react.

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function App() {
+    return (
+        <p>Hello react</p>
+    );
+}
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+console.log("hello pokedex");
+console.log(React);
+console.log(ReactDOM);
+console.log(App);
+console.log(document.getElementById("react-root"));
+
+
+ReactDOM.render(<App />, document.getElementById("react-root"));
