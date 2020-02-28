cd0cfcf1ded2e1eceb75904a853aa80e733e9d87
moru <e1028092@student.tuwien.ac.at>
2020-02-28 19:47:09 +0100

    Working on getting material-ui into the app.
    Working on a topbar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React from "react";
+// import Button from "@material-ui/core/Button"
+import AppBar from "@material-ui/core/AppBar";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from '@material-ui/icons/Menu';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
 
 function App() {
-    return (
-        <p>Hello react</p>
-    );
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          A Title
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
-export default App
+export default App;
