bbb89ca02fe5765f32fe9b2c92aa6c55cfa972dc
moru <e1028092@student.tuwien.ac.at>
2020-02-29 12:46:44 +0100

    Removed normalize. Using material-ui's CssBaseline.

--- src/App.tsx
+++ src/App.tsx
@@ -5,40 +5,47 @@ import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import MenuIcon from '@material-ui/icons/Menu';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import MenuIcon from "@material-ui/icons/Menu";
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
-  root: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginRight: theme.spacing(2)
-  },
-  title: {
-    flexGrow: 1
-  }
-}));
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1
+    }
+  })
+);
 
 function App() {
   const classes = useStyles();
   // TODO title semantically is not an h6
   // TODO no menu icon in overview; only a back button in details view
+  // CssBaseline: a style reset
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          Pokedex
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <React.Fragment>
+      <CssBaseline />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Pokedex
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
   );
 }
 export default App;
