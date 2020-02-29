8bcfa535da98d535324a99112ffb4462a987473a
moru <e1028092@student.tuwien.ac.at>
2020-02-29 20:05:35 +0100

    Enabled concurent-mode enabling react suspense.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {} from "react-dom/experimental"; // import for experimental-mode type-defs
 import App from "./App";
 
-console.log("hello pokedex");
-console.log(React);
-console.log(ReactDOM);
-console.log(App);
-console.log(document.getElementById("react-root"));
+// old sync mode
+// ReactDOM.render(<App />, document.getElementById("react-root"));
 
-ReactDOM.render(<App />, document.getElementById("react-root"));
+// concurrent mode style init; see <https://reactjs.org/docs/concurrent-mode-intro.html>
+const root = document.getElementById("react-root") as HTMLElement;
+ReactDOM.createRoot(root).render(<App />);
