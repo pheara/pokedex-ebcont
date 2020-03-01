6e68e3a534327666748b5f64adbc923e353d95e8
moru <e1028092@student.tuwien.ac.at>
2020-03-01 16:19:54 +0100

    Crude list with loaded pokemon.

--- src/PokemonList.tsx
+++ src/PokemonList.tsx
@@ -1,46 +1,23 @@
 import React, { FunctionComponent } from "react";
 import { delay, wrapPromise, SuspenseResource } from "./utils";
 
-// import Pokedex from "pokeapi-js-wrapper";
+import { getFullPokemonsList } from "./PokeApiWrapper";
+import { PokemonMinimal } from "./model/Pokemon";
 
-declare function require(path: string): object;
-// eslint-disable-next-line no-var-requires
-const Pokedex = require("pokeapi-js-wrapper");
+getFullPokemonsList().then(list => console.log("catch all of these: ", list));
 
-console.log("Pokedex: ", Pokedex);
-
-interface DummyPokemon {
-  id: number;
-  name: string;
-}
-const dummyData: Array<DummyPokemon> = [
-  { name: "A", id: 1 },
-  { name: "B", id: 2 },
-  { name: "C", id: 3 },
-];
-const fetchDummyData = (): {
-  pokemonList: SuspenseResource<Array<DummyPokemon>>;
-} => ({
-  pokemonList: wrapPromise(delay(1500).then(() => dummyData)),
-});
-const failDummyFetching = (): {
-  pokemonList: SuspenseResource<Array<DummyPokemon>>;
-} => ({
-  pokemonList: wrapPromise(
-    delay(1500).then(() => {
-      throw new Error("Failed fetching as intended.");
-    })
-  ),
-});
-
-const resource = Math.random() > 0.5 ? fetchDummyData() : failDummyFetching();
+const resource = wrapPromise(getFullPokemonsList());
 
 const PokemonList: FunctionComponent = () => {
-  const listData: Array<DummyPokemon> = resource.pokemonList.read();
+  const allPokemon: Array<PokemonMinimal> = resource.read();
+
   return (
     <ul>
-      {listData.map((pokemon: DummyPokemon) => (
-        <li key={pokemon.id}>{pokemon.name}</li>
+      {allPokemon.map((pokemon: PokemonMinimal) => (
+        <li key={pokemon.id}>
+          <img src={pokemon.picture.href} alt={pokemon.name} />#{pokemon.id}{" "}
+          {pokemon.name}
+        </li>
       ))}
     </ul>
   );
