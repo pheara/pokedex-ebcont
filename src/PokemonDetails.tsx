96cff522a14f04e3e500057d81b13b6ca3abef7f
moru <e1028092@student.tuwien.ac.at>
2020-03-02 11:00:12 +0100

    Moved pokemon-details into pure component. Checking params for undefined.

--- src/PokemonDetails.tsx
+++ src/PokemonDetails.tsx
@@ -1,21 +1,32 @@
+/* eslint-disable react/prop-types */
 import React, { FunctionComponent } from "react";
 import { useParams } from "react-router-dom";
 import { getPokemonDetailsResourceByName } from "./PokeApiWrapper";
 import { capitalizeFirstLetter } from "./utils";
+import { PokemonDetailed } from "./model/Pokemon";
 
-const PokemonDetails: FunctionComponent = () => {
-  const { name } = useParams();
-  const resource = getPokemonDetailsResourceByName(name);
-  const details = resource.read();
-
+export const PokemonDetailsPure: FunctionComponent<{
+  pokemon: PokemonDetailed;
+}> = ({ pokemon }) => {
+  console.log(pokemon);
   return (
     <React.Fragment>
       <h1>{capitalizeFirstLetter(name)}</h1>
-      {details.types.map(t => (
+      {pokemon.types.map(t => (
         <p key={t.name}>{t.name}</p>
       ))}
     </React.Fragment>
   );
+};
+const PokemonDetails: FunctionComponent = () => {
+  const { name } = useParams();
+  if (!name) {
+    throw new Error("Missing pokemon-name in the URL.");
+  }
+  const resource = getPokemonDetailsResourceByName(name as string);
+  const details = resource.read();
+
+  return <PokemonDetailsPure pokemon={details} />;
 };
 
 export default PokemonDetails;
