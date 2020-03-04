96b61a0d175edf4bad7279ae0e674e59a200abc0
moru <e1028092@student.tuwien.ac.at>
2020-03-04 15:11:54 +0100

    Rudimentary stat-block rendering.

--- src/PokemonDetails.tsx
+++ src/PokemonDetails.tsx
@@ -3,17 +3,25 @@ import React, { FunctionComponent } from "react";
 import { useParams } from "react-router-dom";
 import { getPokemonDetailsResourceByName } from "./PokeApiWrapper";
 import { capitalizeFirstLetter } from "./utils";
-import {
-  PokemonDetailed,
-  NamedUrlResource,
-  MoveViaLevelUp,
-  Move,
-} from "./model/Pokemon";
+
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
+
+import {
+  PokemonDetailed,
+  NamedUrlResource,
+  MoveViaLevelUp,
+  Move,
+  BaseStatBlock,
+} from "./model/Pokemon";
+import { StatBlock } from "./StatBlock";
+
+import { findByLabelText } from "@testing-library/react";
 
 function renderStrDetailList(list: Array<string>): string {
   if (list.length <= 0) {
@@ -54,12 +62,49 @@ const SimpleDetailsListItem: FunctionComponent<{
   );
 };
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    baseStatsItem: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "stretch",
+      alignItems: "stretch",
+    },
+  })
+);
+
+const BaseStatsItem: FunctionComponent<{
+  baseStats: BaseStatBlock;
+}> = ({ baseStats }) => {
+  const classes = useStyles();
+  return (
+    <ListItem className={classes.baseStatsItem}>
+      <Typography variant="body1">Base Stats</Typography>
+      <StatBlock baseStats={baseStats} />
+    </ListItem>
+  );
+};
+const BaseStatsItem2: FunctionComponent<{
+  baseStats: BaseStatBlock;
+}> = ({ baseStats }) => {
+  return (
+    <ListItem>
+      <ListItemText
+        primary="Base Stats"
+        secondary={<StatBlock baseStats={baseStats} />}
+      />
+    </ListItem>
+  );
+};
+
 export const PokemonDetailsPure: FunctionComponent<{
   pokemon: PokemonDetailed;
 }> = ({ pokemon }) => {
   return (
     <React.Fragment>
       <List>
+        <BaseStatsItem baseStats={pokemon.baseStats} />
+        <Divider />
         <SimpleDetailsListItem title="Order" body={"#" + pokemon.order} />
         <Divider />
         <SimpleDetailsListItem
---
+++ src/StatBlock.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable react/prop-types */
+import React, { FunctionComponent } from "react";
+import { BaseStatBlock } from "./model/Pokemon";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+// import { text } from "material-ui/styles/colors";
+
+const useStyles = makeStyles((theme: Theme) => {
+  console.log("theme", theme);
+  return createStyles({
+    gridContainer: {
+      flexGrow: 1,
+      display: "grid",
+      gridTemplateColumns: "1fr 1fr 1fr",
+      gridTemplateRows: "1fr 1fr",
+      gridColumnGap: theme.spacing(2),
+      gridRowGap: theme.spacing(2),
+      //   justifyItems: "center",
+      justifyItems: "start",
+      alignItems: "center",
+    },
+    statTitle: {
+      color: theme.palette.text.secondary,
+    },
+    statValue: {
+      color: theme.palette.text.primary,
+      //   color: text.secondary,
+    },
+  });
+});
+
+export const StatItem: FunctionComponent<{
+  title: string;
+  value: number;
+}> = ({ title, value }) => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Typography variant="body2" className={classes.statTitle}>
+        {title}
+      </Typography>
+      <Typography variant="body2" className={classes.statValue}>
+        {value}
+      </Typography>
+    </div>
+  );
+};
+
+export const StatBlock: FunctionComponent<{
+  baseStats: BaseStatBlock;
+}> = ({ baseStats }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.gridContainer}>
+      <StatItem title="HP" value={baseStats.hp} />
+      <StatItem title="Attack" value={baseStats.attack} />
+      <StatItem title="Defense" value={baseStats.defense} />
+      <StatItem title="Sp. Atk" value={baseStats.specialAttack} />
+      <StatItem title="Sp. Def" value={baseStats.specialDefense} />
+      <StatItem title="Speed" value={baseStats.speed} />
+    </div>
+  );
+};
