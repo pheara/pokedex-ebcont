7b5c9930a4a915edfe17ffe204ec83a4436c10c3
moru <e1028092@student.tuwien.ac.at>
2020-03-06 13:20:59 +0100

    Removed dead code.

--- src/components/PokemonList.tsx
+++ src/components/PokemonList.tsx
@@ -1,12 +1,10 @@
 /* eslint-disable react/prop-types */
-import React, { FunctionComponent, ReactNode } from "react";
+import React, { FunctionComponent } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
-import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import ListItemText from "@material-ui/core/ListItemText";
-import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
 import { FixedSizeList, ListChildComponentProps } from "react-window";
 import AutoSizer from "react-virtualized-auto-sizer";
@@ -23,55 +21,6 @@ import { PokemonMinimal } from "../model/Pokemon";
 getFullPokemonsList().then(list => console.log("catch all of these: ", list));
 
 const resource = getFullPokemonsListResource();
-
-const PokemonList2: FunctionComponent = () => {
-  const allPokemon: Array<PokemonMinimal> = resource.read();
-
-  return (
-    <List>
-      {allPokemon.map((pokemon: PokemonMinimal) => (
-        <ListItem
-          button
-          divider
-          key={pokemon.id}
-          component={RouterLink}
-          to={"/" + pokemon.name}
-          onClick={(): void => {
-            // already start loading
-            getPokemonDetailsResourceByName(pokemon.name);
-          }}
-        >
-          <ListItemAvatar>
-            <img src={pokemon.picture.href} alt={pokemon.name} />
-          </ListItemAvatar>
-          <ListItemText
-            primary={capitalizeFirstLetter(pokemon.name)}
-            secondary={"#" + pokemon.id}
-          />
-        </ListItem>
-      ))}
-    </List>
-  );
-};
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    listItem: {
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-  })
-);
-function renderRow2(props: ListChildComponentProps) {
-  const { index, style } = props;
-  const classes = useStyles();
-  return (
-    <div className={classes.listItem} style={style}>
-      Row {index}
-    </div>
-  );
-}
 
 function renderRow(props: ListChildComponentProps) {
   const { index, style, data } = props;
@@ -121,20 +70,6 @@ const PokemonList: FunctionComponent = () => {
         </FixedSizeList>
       )}
     </AutoSizer>
-
-    //</AutoSizer>
-    // <AutoSizer>
-    //   {({ height, width }): ReactNode => (
-    //     <FixedSizeList
-    //       height={height}
-    //       width={width}
-    //       itemSize={46}
-    //       itemCount={allPokemon.length}
-    //     >
-    //       {renderRow}
-    //     </FixedSizeList>
-    //   )}
-    // </AutoSizer>
   );
 };
 
